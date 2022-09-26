8affffe0b84a84eff16bb2b41394b41ccc0af14b
Khuongpham2001 <phamgiakhuong280601@gmail.com>
2022-09-26 16:34:48 +0700

    Khuong 4:34PM 26/9

--- app/src/main/java/com/example/advance_android_project/Activity/LoginActivity.java
+++ app/src/main/java/com/example/advance_android_project/Activity/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.advance_android_project.Activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
@@ -7,16 +8,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.advance_android_project.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
     Button btn_login;
     TextView textView, tvDeSau;
+    EditText login_email;
+
 
     private FirebaseAuth mAuth;
 
@@ -30,9 +40,26 @@ public class LoginActivity extends AppCompatActivity {
         btn_login = findViewById(R.id.btn_login);
         textView = findViewById(R.id.tvHaveNotAccount);
         tvDeSau = findViewById(R.id.login_tvDeSau);
+        login_email = findViewById(R.id.login_email);
 
-        btn_login.setOnClickListener(v -> {
-
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signInWithEmailAndPassword("khuong@gmail.com","123456")
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                login_email.setText(user.getEmail());
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(LoginActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        });
+            }
         });
 
         textView.setOnClickListener(new View.OnClickListener() {
@@ -48,5 +75,14 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(new Intent(LoginActivity.this, MainActivity.class));
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser user = mAuth.getCurrentUser();
+        if(user != null) {
+            login_email.setText(user.getEmail());
+        }
     }
 }
